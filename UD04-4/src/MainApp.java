2efea12146532c76982d811253a5742ae55592aa
Jimxx22 <jaumegil22@gmail.com>
2022-07-07 09:20:05 +0200

    Enviar ejercicio 4

---
+++ UD04-4/src/MainApp.java
@@ -0,0 +1,15 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a=8;
+		
+		System.out.println("Valor de A= "+ a);
+		System.out.println("Suma: N+77="+ (a+=77));
+		System.out.println("Resta: N-3="+ (a-=3));
+		System.out.println("Multiplicación: N*2="+ (a*=2));
+
+
+	}
+
+}
