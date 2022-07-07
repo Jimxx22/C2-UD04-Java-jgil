07ae7657989e1d5198db3e7a1ea3468702c42084
Jimxx22 <jaumegil22@gmail.com>
2022-07-07 09:18:07 +0200

    Enviar ejercicio 1

---
+++ UD04-1/src/MainApp.java
@@ -0,0 +1,15 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a=8, b=5;
+		
+		System.out.println("Suma: "+a+"+"+b+"="+ (a+b));
+		System.out.println("Resta: "+a+"-"+b+"="+ (a-b));
+		System.out.println("Multiplicación: "+a+"*"+b+"="+ (a*b));
+		System.out.println("División: "+a+"/"+b+"="+ (a/b));
+		System.out.println("Módulo: "+ (a%b));
+		
+	}
+
+}
