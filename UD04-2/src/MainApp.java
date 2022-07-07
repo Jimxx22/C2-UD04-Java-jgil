e7f80505b2b389b06bcda2fe57ae56d06ae4e710
Jimxx22 <jaumegil22@gmail.com>
2022-07-07 09:18:57 +0200

    Enviar ejercicio 2

---
+++ UD04-2/src/MainApp.java
@@ -0,0 +1,18 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int n =17;
+		double a=3.4;
+		char c = 88;
+		
+		System.out.println("Valor N: "+n);
+		System.out.println("Valor A: "+a);
+		System.out.println("Valor C: "+c);
+		System.out.println("Suma: "+n+"+"+a+"="+ (n+a));
+		System.out.println("Resta: "+n+"-"+a+"="+ (n-a));
+		System.out.println("Valor numerico de C: "+Integer.valueOf(c));
+
+	}
+
+}
